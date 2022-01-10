f1c8715596f0486d056cffdcc41fbbbc075f9fb5
JakeCarp <jakecarpenter407@gmail.com>
2022-01-09 18:50:37 -0700

    Basically there, just need to add

---
+++ recipeApp.client/src/services/RecipesService.js
@@ -0,0 +1,34 @@
+import { AppState } from '../AppState';
+import { logger } from '../utils/Logger';
+import {api} from './AxiosService'
+class RecipesService {
+    
+    async GetAllRecipes() {
+        const res = await api.get('api/recipes')
+        logger.log(res.data)
+        AppState.recipes = res.data
+    }
+    async getStepsForRecipe(recipeId) {
+        const res = await api.get('api/recipes/' + recipeId + '/steps')
+        logger.log(res.data)
+        AppState.steps = res.data
+    }
+    async getIngredientsForRecipe(recipeId) {
+        const res = await api.get('api/recipes/' + recipeId + '/ingredients')
+        logger.log(res.data)
+        AppState.ingredients = res.data
+    }
+
+    async editRecipe(recipe) {
+        const res = await api.put('api/recipes/' + recipe.id, recipe)
+        logger.log(res.data)
+        AppState.activeRecipe = res.data
+    }
+
+    async createRecipe(recipe) {
+        const res = await api.post('api/recipes', recipe)
+        logger.log(res.data)
+        AppState.recipes.unshift(res.data)
+    }
+}
+export const recipesService = new RecipesService();
